25608ffaefd0c2334a7d4e368b590fd89900e1a6
rifrifqyqy <rifqyh22@gmail.com>
2024-12-01 20:47:50 +0700

    create components button

---
+++ src/lib/components/elements/Button.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { twMerge } from 'tailwind-merge';
+
+	export let label = 'Click me';
+	export let onClick = () => {};
+	export let customClass = '';
+	export let style = '';
+</script>
+
+<button
+	on:click={onClick}
+	class={twMerge(
+		`button bg-primary-100 rounded-full px-4 py-2 text-white transition-all duration-300 ${customClass}`
+	)}
+	{style}
+>
+	<slot>{label}</slot>
+</button>
+
+<style>
+	.button {
+		font-size: 1rem;
+		cursor: pointer;
+		height: fit-content;
+		display: flex;
+	}
+
+	.button:hover {
+		@apply opacity-80;
+	}
+</style>
