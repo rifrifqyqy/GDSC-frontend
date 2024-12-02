c5413595042d04444ad7d8f155a9e1f80b59db72
rifrifqyqy <rifqyh22@gmail.com>
2024-12-03 00:06:38 +0700

    slicing our menu section

---
+++ src/lib/components/layouts/Herosection/menuShow.svelte
@@ -0,0 +1,23 @@
+<script>
+	let images = [
+		{
+			id: 1,
+			url: '/images/fatman1.png'
+		}
+	];
+</script>
+
+<main>
+	<section>
+		<p>Our Menu</p>
+		<article>Menu That Always <br /> Make You Fall In Love</article>
+	</section>
+	<section>
+		<img src="" alt="" class="menu-img" />
+	</section>
+</main>
+
+<style scoped>
+	.menu-img {
+	}
+</style>
