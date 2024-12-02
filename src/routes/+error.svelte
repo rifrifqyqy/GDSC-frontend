06074107a759ec65cac06c4c968fdbd095617e0c
rifrifqyqy <rifqyh22@gmail.com>
2024-12-03 00:06:11 +0700

    slicing error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,27 +1,36 @@
 <script>
-	export let error;
-	export let status;
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import Button from '$lib/components/elements/Button.svelte';
 </script>
 
-<main class="text-center">
-	<h1>{status}</h1>
-	<p>{error.message}</p>
-	{#if status === 404}
-		<p>Halaman tidak ditemukan. <a href="/">Kembali ke beranda</a></p>
-	{/if}
+<main class="error-page">
+	<div class="wrapper">
+		<h1>{`${$page.status}`}</h1>
+		<p class="err-status">{$page.error?.message || 'Something went wrong.'}</p>
+
+		{#if $page.status === 404}
+			<p class="flex flex-col items-center gap-2">
+				Halaman tidak ditemukan. Silakan kembali ke <Button
+					label="Beranda"
+					onClick={() => goto('/')}
+				/>
+			</p>
+		{/if}
+	</div>
 </main>
 
-<style scoped>
-	main {
-		margin: 2rem;
-		font-family: Arial, sans-serif;
-	}
-	h1 {
-		font-size: 3rem;
-		color: red;
-	}
-	a {
-		color: blue;
-		text-decoration: underline;
+<style>
+	.error-page {
+		@apply flex min-h-screen flex-col items-center justify-center gap-6;
+		h1 {
+			@apply bg-primary-100 w-fit p-12 text-6xl font-semibold uppercase text-white;
+		}
+		.err-status {
+			@apply text-xl font-semibold uppercase text-gray-600;
+		}
+		.wrapper {
+			@apply m-auto flex flex-col items-center gap-4;
+		}
 	}
 </style>
