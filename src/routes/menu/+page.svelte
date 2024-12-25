1055cb080a127816c243390e097788b9eb439bbd
rifrifqyqy <rifqyh22@gmail.com>
2024-12-25 20:16:32 +0700

    feat: navigation menu:id

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -20,13 +20,15 @@
 	</header>
 	<section class="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-10 lg:grid-cols-4 xl:grid-cols-5">
 		{#each recipes as R}
-			<CardMenu
-				title={R.name}
-				thumb={R.image}
-				label={R.mealType}
-				rating={R.rating}
-				price={formatToRupiah(R.caloriesPerServing * 100)}
-			/>
+			<a href={`/menu/${R.id}`}>
+				<CardMenu
+					title={R.name}
+					thumb={R.image}
+					label={R.mealType}
+					rating={R.rating}
+					price={formatToRupiah(R.caloriesPerServing * 100)}
+				/>
+			</a>
 		{/each}
 	</section>
 </main>
