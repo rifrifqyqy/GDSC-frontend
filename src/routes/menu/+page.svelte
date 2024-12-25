86709bc6307203bac2e824eb68c2cef04652320b
rifrifqyqy <rifqyh22@gmail.com>
2024-12-25 19:52:25 +0700

    fix responsive wrapper

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -11,14 +11,14 @@
 	<meta name="description" content="Fudo website" />
 </svelte:head>
 
-<main class="main-wrapper">
+<main class="menu-wrapper">
 	<header
 		class="bg-primary-100/15 mb-4 mt-1 flex items-center gap-4 rounded-2xl p-4 md:my-8 md:p-8"
 	>
-		<img src="./images/fudologo.png" class="h-8 md:h-12" alt="" />
+		<img src="./images/fudologo.png" class="h-6 md:h-12" alt="" />
 		<h1 class="font-semibold">Fudo Menu</h1>
 	</header>
-	<section class="grid grid-cols-2 gap-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+	<section class="grid grid-cols-2 gap-4 md:gap-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
 		{#each recipes as R}
 			<CardMenu
 				title={R.name}
