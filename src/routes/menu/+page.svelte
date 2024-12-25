3f2a028bc34b4ecebe7026b4dd26156e8ca52387
rifrifqyqy <rifqyh22@gmail.com>
2024-12-25 18:47:24 +0700

    feat: get api menus | mapping datas

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,8 +1,33 @@
+<script>
+	import CardMenu from '$lib/components/elements/CardMenu.svelte';
+
+	export let data;
+	const { recipes } = data;
+	console.log(recipes);
+</script>
+
 <svelte:head>
-  <title>FUDO | Menu Pages</title>
-  <meta name="description" content="Fudo website" />
+	<title>FUDO | Menu Pages</title>
+	<meta name="description" content="Fudo website" />
 </svelte:head>
 
-<main>
-	<h1>Menu Pages</h1>
+<main class="main-wrapper">
+	<header class="bg-primary-100/15 my-8 flex items-center gap-4 rounded-2xl p-8">
+		<img src="./images/fudologo.png" class="h-12" alt="" />
+		<h1 class="font-semibold">Fudo Menu</h1>
+	</header>
+	<section class="grid grid-cols-5 gap-10">
+		{#each recipes as R}
+			<CardMenu
+				title={R.name}
+				thumb={R.image}
+				label={R.mealType}
+				rating={R.rating}
+				price="${R.caloriesPerServing}"
+			/>
+		{/each}
+	</section>
 </main>
+
+<style>
+</style>
