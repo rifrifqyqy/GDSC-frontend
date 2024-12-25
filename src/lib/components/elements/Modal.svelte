d53fb4b7f9a884e7ff0ba005147f70e8102b99f6
rifrifqyqy <rifqyh22@gmail.com>
2024-12-25 22:36:05 +0700

    feat: adding modal loading

---
+++ src/lib/components/elements/Modal.svelte
@@ -0,0 +1,8 @@
+<div class="fixed inset-0 z-[9999] flex items-center justify-center bg-black/60">
+	<div
+		class="flex aspect-square h-44 flex-col justify-center gap-8 rounded-xl bg-white p-8 md:h-64"
+	>
+		<img src="/images/fudologo.png" class="mx-auto max-md:h-20" alt="" />
+		<p class="text-center font-semibold">Loading...</p>
+	</div>
+</div>
