3c58d9a70b260d3661c9364233d499e94d75892a
rifrifqyqy <rifqyh22@gmail.com>
2024-12-28 18:55:26 +0700

    style: temp style unconstructed pages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,6 +3,8 @@
 	<meta name="description" content="Fudo website" />
 </svelte:head>
 
-<main>
-	<h1>Contact Pages</h1>
+<main class="flex h-dvh w-full p-4">
+	<h1 class="bg-primary-100/15 flex w-full rounded-2xl">
+		<p class="text-primary-100 m-auto font-semibold uppercase">Contact Page</p>
+	</h1>
 </main>
--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -3,6 +3,8 @@
 	<meta name="description" content="Fudo website" />
 </svelte:head>
 
-<main>
-	<h1>Services Pages</h1>
+<main class="flex h-dvh w-full p-4">
+	<h1 class="bg-primary-100/15 w-full flex rounded-2xl">
+		<p class="m-auto font-semibold text-primary-100 uppercase">Services Page</p>
+	</h1>
 </main>
