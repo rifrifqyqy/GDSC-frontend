a9a7b84325741ec3bcb67e44f5bdece01d85ff10
rifrifqyqy <rifqyh22@gmail.com>
2024-12-28 16:17:26 +0700

    hotfix: fix issue data not reload while clicking card

--- src/lib/components/layouts/Menu/MenuRecom.svelte
+++ src/lib/components/layouts/Menu/MenuRecom.svelte
@@ -1,11 +1,13 @@
 <script>
+	import { goto } from '$app/navigation';
 	import CardMenu from '$lib/components/elements/CardMenu.svelte';
 
 	// buat props fiveData
-	export let fiveData = [];
+	export let fiveData;
 	import { formatToRupiah } from '$lib/utils/currency';
 </script>
 
+ad
 <main class="mt-8 flex flex-col gap-6 md:mt-16">
 	<header>
 		<h1 class=" text-[1.3rem] font-semibold md:text-[2rem]">You may also like</h1>
@@ -13,7 +15,7 @@
 	<section class=" grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-16 lg:grid-cols-4 xl:grid-cols-5">
 		<!-- loop max 5 datas -->
 		{#each fiveData as data}
-			<a href="/menu/{data.id}">
+			<a href={`/menu/${data.id}`} data-sveltekit-reload>
 				<CardMenu
 					title={data.name}
 					thumb={data.image}
@@ -21,6 +23,7 @@
 					rating={data.rating}
 					price={formatToRupiah(data.caloriesPerServing * 100)}
 				/>
+				{data.id}
 			</a>
 		{/each}
 	</section>
