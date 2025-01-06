53df0cd22e89306d75a93970830c7ad688e3b8bc
rifrifqyqy <rifqyh22@gmail.com>
2025-01-06 19:13:37 +0700

    feat: add service page

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,10 +1,1 @@
-<svelte:head>
-	<title>FUDO | Services Page</title>
-	<meta name="description" content="Fudo website" />
-</svelte:head>
-
-<main class="flex h-dvh w-full p-4">
-	<h1 class="bg-primary-100/15 w-full flex rounded-2xl">
-		<p class="m-auto font-semibold text-primary-100 uppercase">Services Page</p>
-	</h1>
-</main>
+<main>Service</main>
